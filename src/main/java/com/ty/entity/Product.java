dde265cd87ac716eb4f16c81eb69f6172624f8f4
Harshu-1999 <harshalpatil8108@gmail.com>
2024-11-26 15:46:29 +0530

    first commit

---
+++ src/main/java/com/ty/entity/Product.java
@@ -0,0 +1,58 @@
+package com.ty.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String name;
+    private Double price;
+
+    // Many Products can belong to one Category
+    @ManyToOne
+    @JoinColumn(name = "category_id") // Foreign key to Category
+    private Category category;
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
+
